62d4ed3eea97cf90d87aae99f1fa6bb081023c2c
Tamar Khojamirova <tamarkhojamirovi@Tamars-Air.home>
2020-03-20 11:05:29 +0100

    cteating components

--- working-with-lists/src/app/app.module.ts
+++ working-with-lists/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { StudentComponent } from './student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentListComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule
