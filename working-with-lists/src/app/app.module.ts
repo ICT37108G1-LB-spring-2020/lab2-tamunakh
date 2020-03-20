7157147ff1e781052d98fa1e609be1b890c8fe12
Tamar Khojamirova <tamarkhojamirovi@Tamars-Air.home>
2020-03-20 12:13:52 +0100

    lab2-done

--- working-with-lists/src/app/app.module.ts
+++ working-with-lists/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { StudentListComponent } from './student-list/student-list.component';
-import { StudentComponent } from './student/student.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentListComponent,
-    StudentComponent
   ],
   imports: [
     BrowserModule
--- working-with-lists/src/app/student-list/student-list.component.ts
+++ working-with-lists/src/app/student-list/student-list.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-list.component.css']
 })
 export class StudentListComponent implements OnInit {
+  infoTitles: string[];
+  students: string[][];
 
-  constructor() { }
+  constructor() {
+    this.infoTitles = ['Name', 'Surname', 'ID'];
+    this.students = [
+      ['John', 'Doe', '01234567890'],
+      ['Jane', 'Smith', '98765432100'],
+      ['Ann', 'Miller', '23567985123'],
+      ['Dave', 'Davis', '56319758431']
+    ];
+  }
 
   ngOnInit(): void {
   }
-
 }
